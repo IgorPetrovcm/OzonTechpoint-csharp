8ab5fe09ef1951b10000da686f5938490a705f55
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-23 16:50:20 +0300

    Ready solution, прошел проверку, результат: 56 мс, 23 мб

---
+++ SeaBattle/ReadySolution/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ReadySolution;
+
+
+public class Program
+{
+    static void Main()
+    {
+        int countLines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countLines; i++)
+        {
+            int[] numbers = Console.ReadLine()
+                                .Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            int[] trueNumbers = {4,3,2,1};
+
+            for (int j = 0; j < 10; j++)
+            {
+                trueNumbers[numbers[j] - 1]--;
+            }
+
+            bool isTrue = true;
+
+            for (int j = 0; j < 4; j++)
+            {
+                if (trueNumbers[j] != 0)
+                {
+                    System.Console.WriteLine("no");
+
+                    isTrue = false;
+
+                    break;
+                }
+            }
+
+            if (isTrue)
+                System.Console.WriteLine("yes");
+        }
+    }
+}
