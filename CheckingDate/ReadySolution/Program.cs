f289972db0eaed5666ab7e3e246601613f086887
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-23 20:42:57 +0300

    _

---
+++ CheckingDate/ReadySolution/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReadySolution;
+
+
+public class Program
+{
+    static void Main()
+    {
+        int countLines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countLines; i++)
+        {
+            ReadOnlySpan<char> dateParams = Console.ReadLine();
+
+            DateTime isDate;
+
+            if (DateTime.TryParse(dateParams, out isDate))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
