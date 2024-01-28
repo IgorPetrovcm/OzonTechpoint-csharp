147f5a0a81bfa5827d67d0a6cb0c8c1316ea164b
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-28 16:44:03 +0300

    Готовое решение: 123мс, 25мб

---
+++ CarNumbers/ReadySolution/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace ReadySolution;
+
+
+public class Program 
+{
+    static void Main()
+    {
+        int taskCount = int.Parse(Console.ReadLine());
+
+        string pattern = @"\D{1}\d{1,2}\D{2}";
+
+        for (int i = 0; i < taskCount; i++)
+        {
+            string task = Console.ReadLine();
+
+            string[] matchesLine = new string[task.Length / 4];
+
+            int j = 0;
+
+            foreach (Match match in Regex.Matches(task,pattern))
+            {
+                task = task.Replace(match.ToString(), "");
+
+                matchesLine[j] = match.ToString();
+
+                j++;
+            }
+
+            if (task.Length > 0)
+                Console.WriteLine("-");
+            else 
+                Console.WriteLine(String.Join(" ", matchesLine));
+        }
+    }
+}
