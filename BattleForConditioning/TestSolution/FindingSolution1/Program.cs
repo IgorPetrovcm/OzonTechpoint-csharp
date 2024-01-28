43bc51ddc93ab920731c14139c1ab4b73ebef710
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-28 21:55:24 +0300

    один из вариантов решения, пока что единственный и лучший

--- BattleForConditioning/TestSolution/FindingSolution1/Program.cs
+++ BattleForConditioning/TestSolution/FindingSolution1/Program.cs
@@ -9,21 +9,59 @@ public class Program
     {
         OzonCurrentTest test = OzonTools.CompleteCreation(File.ReadAllLines("Task.txt"),File.ReadAllLines("Result.txt"));
 
-        for (int i = 0; i < test.TaskCount; i++)
+        string[] task = test.ArrayTasks;
+
+        YourTaskResult result = new YourTaskResult();
+
+        for (int j = 0; j < task.Length; j++)
         {
-            string[] task = test.ArrayTasks;
+            int[] interval = {15, 30};
 
-            YourTaskResult result = new YourTaskResult();
+            bool isError = false;
 
-            for (int j = 0; j < task.Length; j++)
+            int employeeCount = int.Parse(task[j]);
+
+            for (int h = 0; h < employeeCount; h++)
             {
-                for (int h = 0; h < int.Parse(task[j]); h++)
-                {
-                    j++;
+                j++;
 
-                    
+                if (isError == true)
+                {
+                    result.Add("-1");
+
+                    continue;
                 }
+
+                if (task[j][0] == '>')
+                {
+                    int newIntervalNumber = int.Parse(task[j][3].ToString() + task[j][4].ToString());
+
+                    if (newIntervalNumber > interval[0])
+                        interval[0] = newIntervalNumber;
+                }
+                else 
+                {
+                    int newIntervalNumber = int.Parse(task[j][3].ToString() + task[j][4].ToString());
+
+                    if (newIntervalNumber < interval[1])
+                        interval[1] = newIntervalNumber;
+                }
+
+                if (interval[1] < interval[0])
+                {
+                    isError = true;
+
+                    result.Add("-1");
+
+                    continue;
+                }
+
+                result.Add(interval[0].ToString());
             }
+
+            result.Add("");
         }
+
+        System.Console.WriteLine(test.TestChecking(result));
     }
 }
