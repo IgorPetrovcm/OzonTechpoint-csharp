84a15ba926379bcc6e9e5d406cd6953bbe7f78cb
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-29 22:10:52 +0300

    Демонстрация новой возможности библиотеки OzonTestsManager, теперь есть примитив гибкой настройки теста

---
+++ DataCompression/TestSolution/TemplateForTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TemplateForTesting;
+
+using OzonTestsManager;
+
+
+public class Program
+{
+    static void Main()
+    {
+        //Так как привычный способ создания объекта текущего теста подразумевает инкапсуляцию значения количество наборов данных
+        //в первой строке документа с заданием, то нам придеться использовать более гибкий способ создания 
+
+        OzonTasksBuilder tasksBuilder = new OzonTasksBuilder();
+
+        tasksBuilder.SetTask(TaskSettings.IgnoreTitle, File.ReadAllLines("Task.txt"));
+
+        OzonCurrentTest test = tasksBuilder.Build();
+
+        
+    }
+}
