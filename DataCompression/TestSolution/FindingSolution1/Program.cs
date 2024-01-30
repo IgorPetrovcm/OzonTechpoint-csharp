b5ca4c90185305d23b45a2dd2396f176326c29c7
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-30 12:38:02 +0300

    prototype solution

--- DataCompression/TestSolution/FindingSolution1/Program.cs
+++ DataCompression/TestSolution/FindingSolution1/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace FindingSolution1;
 
 using System.Globalization;
+using System.Text;
+using System.Xml.XPath;
 using OzonTestsManager;
 
 
@@ -8,10 +10,7 @@ public class Program
 {
     static void Main()
     {
-        OzonTasksBuilder builder = new OzonTasksBuilder();
-        builder.SetTask(TaskSettings.IgnoreTitle, File.ReadAllLines("Task.txt"));
-
-        OzonCurrentTest test = builder.Build();
+        OzonCurrentTest test = OzonTools.CompleteCreation(File.ReadAllLines("Task.txt"), File.ReadAllLines("Result.txt"));
 
         string[] task = test.ArrayTasks;
 
@@ -20,11 +19,49 @@ public class Program
             int[] numbers = task[i + 1]
                                 .Split(' ').Select(x => int.Parse(x)).ToArray();
 
+            StringBuilder result = new StringBuilder();
+
+            int current = 0;
+
+            bool isMore = false;
+
+            int pre = 0;
+
+            int pos = 0;
+
             for (int j = 1; j < int.Parse(task[i]); j++)
             {
-                if (Math.Abs(numbers[j] - numbers[j-1]) == 1)
+                if (current == 0)
                 {
-                    if 
+                    if (Math.Abs(numbers[j] - numbers[j-1]) == 1)
+                    {
+                        current++;
+
+                        pre = numbers[j-1];
+
+                        if (numbers[j] < numbers[j-1])
+                        {
+                            isMore = false;
+                        }
+                        else 
+                        {
+                            isMore = true;
+                        }
+                    }
+                }
+                else if (Math.Abs(numbers[j] - numbers[j-1]) == 1)
+                {
+                    if ((numbers[j] > numbers[j-1]) == isMore)
+                    {
+                        current++;
+                    }
+                    else
+                    {
+                        pos = numbers[j];
+
+
+                        result.Append(pre + " " + pos);
+                    }
                 }
             }
         }
