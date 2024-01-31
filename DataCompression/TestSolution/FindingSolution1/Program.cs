c4444b0738060ab41683445d1ff9805fa1d29c22
Honor <igorpetrov42ru@gmail.com>
2024-01-31 13:38:26 +0300

    Сохранить изменение, среда для реализации псевдорешения алгоритма

--- DataCompression/TestSolution/FindingSolution1/Program.cs
+++ DataCompression/TestSolution/FindingSolution1/Program.cs
@@ -8,85 +8,13 @@ public class Program
 {
     static void Main()
     {
-        OzonCurrentTest test = OzonTools.CompleteCreation(File.ReadAllLines("../../../Task.txt"), File.ReadAllLines("../../../Result.txt"));
+        OzonTasksBuilder builder = 
+                OzonTools.CreateBuilderWithBasicSettings(TaskSettings.IgnoreTitle, 
+                        File.ReadAllLines("Task.txt"), File.ReadAllLines("Result.txt")
+                    );
+
+        OzonCurrentTest test = builder.Build();
 
         string[] task = test.ArrayTasks;
-
-        for (int i = 0; i < task.Length; i++)
-        {
-            int[] numbers = task[i + 1]
-                                .Split(' ').Select(x => int.Parse(x)).ToArray();
-
-            StringBuilder result = new StringBuilder();
-
-            int current = 0;
-
-            bool isMore = false;
-
-            int pre = 0;
-
-            for (int j = 1; j < int.Parse(task[i]); j++)
-            {
-                if (current == 0)
-                {
-                    if (Math.Abs(numbers[j] - numbers[j-1]) == 1)
-                    {
-                        pre = numbers[j-1];
-
-                        if (numbers[j] < numbers[j-1])
-                        {
-                            isMore = false;
-                        }
-                        else 
-                        {
-                            isMore = true;
-                        }
-                    }
-                    else 
-                    {
-                        pre = numbers[j-1];
-
-                        result.Append(pre + " " + "0 ");
-                    }
-                }
-                if (Math.Abs(numbers[j] - numbers[j-1]) == 1)
-                {
-                    if ((numbers[j] > numbers[j-1]) == isMore)
-                    {
-                        current++;
-                    }
-                    else
-                    {
-                        result.Append(IsMoreResult(pre, current, isMore));
-
-                        current = 0;
-                    }
-                }
-                if (j == int.Parse(task[i]) - 1)
-                {
-                    result.Append(IsMoreResult(pre, current, isMore));
-
-                    System.Console.WriteLine(result.ToString());
-                }
-            }
-
-            i++;
-        }
-
-        string IsMoreResult(int pre, int current, bool isMore)
-        {
-            int pos = 0;
-
-            if (isMore == true)
-            {
-                pos = current;
-            }
-            else
-            {
-                pos = -current;
-            }
-
-            return pre + " " + pos + " ";
-        }
     }
 }
