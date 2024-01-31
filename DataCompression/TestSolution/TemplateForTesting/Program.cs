5483f145350a561ac9d0f43f6b7d6e21e53c2f40
Honor <igorpetrov42ru@gmail.com>
2024-01-31 13:26:45 +0300

    Update Template from solution because builder system is updated

--- DataCompression/TestSolution/TemplateForTesting/Program.cs
+++ DataCompression/TestSolution/TemplateForTesting/Program.cs
@@ -10,12 +10,11 @@ public class Program
         //Так как привычный способ создания объекта текущего теста подразумевает инкапсуляцию значения количество наборов данных
         //в первой строке документа с заданием, то нам придеться использовать более гибкий способ создания 
 
-        OzonTasksBuilder tasksBuilder = new OzonTasksBuilder();
-
-        tasksBuilder.SetTask(TaskSettings.IgnoreTitle, File.ReadAllLines("Task.txt"));
-
-        OzonCurrentTest test = tasksBuilder.Build();
-
+        OzonTasksBuilder tasksBuilder = 
+                OzonTools.CreateBuilderWithBasicSettings(TaskSettings.IgnoreTitle,
+                         File.ReadAllLines("Task.txt"),File.ReadAllLines("Result.txt")
+                    );
         
+        OzonCurrentTest test = tasksBuilder.Build();
     }
 }
